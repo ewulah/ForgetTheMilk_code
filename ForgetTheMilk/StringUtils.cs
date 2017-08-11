13160d92ba05ba71c8e44c263035afdc2f3ab8e8
Ewa <ewulah@yahoo.com>
2017-08-11 16:28:43 -0400

    added tests; config changes

---
+++ ForgetTheMilk/StringUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForgetTheMilk
+{
+    public class StringUtils
+    {
+        public string MakeUpper(string s)
+        {
+            return s.ToUpper();
+        }
+
+        public string AppendNumberToString(string s, int num)
+        {
+            return s + num;
+        }
+    }
+}
